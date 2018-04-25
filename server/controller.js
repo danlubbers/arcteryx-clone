e65225f4b814ea8b31ba5906ce821c50c9bd2238
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-25 14:33:32 -0600

    added promiseMiddleware in store.js

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,15 @@
 module.exports = {
     products: (req, res) => {
         const dbInstance = req.app.get('db');
-
-        dbInstance.getAllProducts().then(products => res.status(200).send(products)).catch((err)=>res.status(500).send(err))
+        // References 'getAllProducts.sql file in (db) folder
+        dbInstance.products.getAllProducts().then(products => res.status(200).send(products)).catch((err)=>res.status(500).send(err))
     },
 
     product: (req, res) => {
         // console.log('word')
         const dbInstance = req.app.get('db');
-
-        dbInstance.getOneProduct(req.params.id).then(productID => res.status(200).send(productID)).catch((err)=>res.status(500).send(err))
+        // References 'getOneProduct.sql file in (db) folder
+        // Must have (req.params.id) to get the id and pass it through
+        dbInstance.products.getOneProduct(req.params.id).then(productID => res.status(200).send(productID)).catch((err)=>res.status(500).send(err))
     } 
 }
--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,6 +1,11 @@
 // Always impoert {createStore} and render
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
+// import to use promiseMiddleware
+import promiseMiddleware from 'redux-promise-middleware';
+
+// put promiseMiddleware() ont a variable called middleware
+let middleware = promiseMiddleware();
 
 // Export createStore by default and pass it 'reducer' as it's arguement
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(middleware));
