607e41d9a841ce8828d2ef3c6d60fd331c4a5fa7
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-25 20:02:26 -0600

    cart page styled minimally

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -18,10 +18,22 @@ export default class Cart extends Component {
                     <h5>PRICE</h5>
                     <h5>REMOVE</h5>
                 </div>
-                <div>
-                    <h5>DUMMY DATA</h5>
-                    <h5>DUMMY DATA</h5>
-                    <h5>DUMMY DATA</h5>
+                <div className="product-content">
+                    <h5>Alpha SV</h5>
+                    <h5>1</h5>
+                    <h5>749.00</h5>
+                    <h5>Remove button</h5>
+                </div>
+                <div className="cart-container">
+                    {/* <h5>CART CONTENT</h5> */}
+                    <div className="cart-content">
+                        <h5 className="item">Item Total:</h5>
+                        <h5 className="express">Express Shipping:</h5>
+                        <h5 className="total">Total:</h5>
+                    </div>
+                    <div className="checkoutBtn-container"> 
+                        <button className="checkoutBtn">CONTINUE CHECKOUT</button>
+                    </div>
                 </div>
             </div>
         )
