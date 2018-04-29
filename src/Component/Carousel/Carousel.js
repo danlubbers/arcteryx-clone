d8a5a5e57c1582bb65ffde7159b6ecb237b04960
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-29 14:46:08 -0600

    changed layout of homepage images and added feature button functionality

--- src/Component/Carousel/Carousel.js
+++ src/Component/Carousel/Carousel.js
@@ -5,6 +5,7 @@ import assault from '../../images/assault.jpg';
 import insulation from '../../images/insulation.jpg';
 import midlayers from '../../images/midlayers.jpg';
 import xFunctional from '../../images/x-functional.jpg';
+import {Link} from 'react-router-dom'
 
 export default class Carousel extends Component {
 
@@ -31,25 +32,25 @@ export default class Carousel extends Component {
             <Slider ref={slider => (this.slider = slider)}{...settings}>
             <div>
                 <div className="text-box">
-                    <h1 className="text-category">Enhanced Mobility</h1>
-                    <h1 className="text-title">Assault Uniform LT</h1>
-                    <p className="text-description">Full range of motion combat uniform for hot weather conditions</p>
+                    <h1 className="text-category">Extreme Cold Weather Kit</h1>
+                    <h1 className="text-title">New Cold WX Parka & BIB Pant SVX </h1>
+                    <p className="text-description">For the operational needs of personnel in extreme cold weather conditions.</p>
                 <div className="button-container">
-                    <button className="text-button">Assault Shirt LT</button>
-                    <button className="text-button">Assault Pant LT</button>
+                    <Link to={`/product/1`}><button className="text-button">View Parka SVX</button></Link>
+                    <Link to={`/product/8`}><button className="text-button">View Bib Pant SVX</button></Link>
                 </div>
                 </div>
+                    <img className="insulation-img" src={insulation} alt="insulation"/>
+                </div>
+                <div>
                     <img className="assault-img" src={assault} alt="assault"/>
                 </div>
-            <div>
-                <img className="insulation-img" src={insulation} alt="insulation"/>
-            </div>
-            <div>
-                <img className="midlayers-img" src={midlayers} alt="midlayers"/>
-            </div>
-            <div>
-                <img className="x-functional-img" src={xFunctional} alt="x-functional"/>
-            </div>
+                <div>
+                    <img className="midlayers-img" src={midlayers} alt="midlayers"/>
+                </div>
+                <div>
+                    <img className="x-functional-img" src={xFunctional} alt="x-functional"/>
+                </div>
 
 
             </Slider>
