6418882f84e3d32df92bec5e52c4a59ae4d08c7f
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-29 14:07:22 -0600

    autoplay is working and added overlay to homepage image

--- src/Component/Carousel/Carousel.js
+++ src/Component/Carousel/Carousel.js
@@ -20,18 +20,27 @@ export default class Carousel extends Component {
         speed: 500,
         slidesToShow: 1,
         slidesToSlide: 4,
-        autoPlay: true,
+        // autoplay: true,
         autoPlaySpeed: 1000,
         // cssEase: "linear",
         // adaptiveHeight: true,
         initialSlide: 0
     };
     return (
-        <div>
+        <div className="carousel">
             <Slider ref={slider => (this.slider = slider)}{...settings}>
             <div>
-                <img className="assault-img" src={assault} alt="assault"/>
-            </div>
+                <div className="text-box">
+                    <h1 className="text-category">Enhanced Mobility</h1>
+                    <h1 className="text-title">Assault Uniform LT</h1>
+                    <p className="text-description">Full range of motion combat uniform for hot weather conditions</p>
+                <div className="button-container">
+                    <button className="text-button">Assault Shirt LT</button>
+                    <button className="text-button">Assault Pant LT</button>
+                </div>
+                </div>
+                    <img className="assault-img" src={assault} alt="assault"/>
+                </div>
             <div>
                 <img className="insulation-img" src={insulation} alt="insulation"/>
             </div>
