a23ab236e3394c726ed207e61f19c7c27a8df718
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-28 19:34:57 -0600

    carousel flick pans, but does not autoplay yet

--- src/Component/Carousel/Carousel.js
+++ src/Component/Carousel/Carousel.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
+import './Carousel.css';
 import assault from '../../images/assault.jpg';
 import insulation from '../../images/insulation.jpg';
 import midlayers from '../../images/midlayers.jpg';
 import xFunctional from '../../images/x-functional.jpg';
 
 export default class Carousel extends Component {
+
+    // componentDidMount() {
+    //         this.slider.slickPlay();
+    //     }
+
     render() {
     const settings = {
         dots: true, 
+        arrows: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 4,
-        slidesToSlide: 1
+        slidesToShow: 1,
+        slidesToSlide: 4,
+        autoPlay: true,
+        autoPlaySpeed: 1000,
+        // cssEase: "linear",
+        // adaptiveHeight: true,
+        initialSlide: 0
     };
     return (
         <div>
-            <Slider {...settings}>
+            <Slider ref={slider => (this.slider = slider)}{...settings}>
             <div>
                 <img className="assault-img" src={assault} alt="assault"/>
             </div>
