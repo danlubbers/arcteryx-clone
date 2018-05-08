a5886b204733efa144d852db55caba734461f3e6
Dan Lubbers <lubbers.dan@gmail.com>
2018-05-08 14:07:12 -0600

    separated scss into components with partials from sass

--- src/Component/Carousel/Carousel.js
+++ src/Component/Carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Slider from 'react-slick';
-import './Carousel.css';
 import assault from '../../images/assault.jpg';
 import insulation from '../../images/insulation.jpg';
 import midlayers from '../../images/midlayers.jpg';
--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,16 +1,10 @@
 import React from 'react';
-// import backgroundDashboardImg from '../../images/midlayers.jpg';
 import Carousel from '../Carousel/Carousel';
 
 export default function Dashboard() {
     return(
         <div>
            <Carousel />
-            {/* <div>
-                <img className="midlayers-img"src={backgroundDashboardImg} alt="background images"/> 
-            </div> */}
-
-           
         </div>
     )
 }
